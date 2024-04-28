e6c60cb070984e8ec5b9eda4089af678961cd9b6
Hidetake Iwata <int128@gmail.com>
2024-04-28 17:56:03 +0900

    Fix octokit plugin

--- src/github.ts
+++ src/github.ts
@@ -3,4 +3,4 @@ import * as pluginRetry from '@octokit/plugin-retry'
 
 export type Octokit = ReturnType<typeof github.getOctokit>
 
-export const getOctokit = (token: string): Octokit => github.getOctokit(token, pluginRetry.retry)
+export const getOctokit = (token: string): Octokit => github.getOctokit(token, {}, pluginRetry.retry)
