7472e07e08a48dab0fa53a240b3f6ce742409d48
update-generated-files-action <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-18 07:52:06 +0000

    Generated by GitHub Actions (ts / generate)
    https://github.com/int128/wait-for-workflows-action/actions/runs/18612871689

--- src/generated/graphql.ts
+++ src/generated/graphql.ts
@@ -10,4 +10,9 @@ export type ListChecksQueryVariables = Types.Exact<{
 }>;
 
 
-export type ListChecksQuery = { __typename?: 'Query', rateLimit?: { __typename?: 'RateLimit', cost: number, remaining: number } | null, repository?: { __typename?: 'Repository', object?: { __typename: 'Blob' } | { __typename: 'Commit', checkSuites?: { __typename?: 'CheckSuiteConnection', totalCount: number, pageInfo: { __typename?: 'PageInfo', hasNextPage: boolean, endCursor?: string | null }, nodes?: Array<{ __typename?: 'CheckSuite', status: Types.CheckStatusState, conclusion?: Types.CheckConclusionState | null, workflowRun?: { __typename?: 'WorkflowRun', event: string, url: string, workflow: { __typename?: 'Workflow', name: string } } | null } | null> | null } | null } | { __typename: 'Tag' } | { __typename: 'Tree' } | null } | null };
+export type ListChecksQuery = { __typename?: 'Query', rateLimit?: { __typename?: 'RateLimit', cost: number, remaining: number } | null, repository?: { __typename?: 'Repository', object?:
+      | { __typename: 'Blob' }
+      | { __typename: 'Commit', checkSuites?: { __typename?: 'CheckSuiteConnection', totalCount: number, pageInfo: { __typename?: 'PageInfo', hasNextPage: boolean, endCursor?: string | null }, nodes?: Array<{ __typename?: 'CheckSuite', status: Types.CheckStatusState, conclusion?: Types.CheckConclusionState | null, workflowRun?: { __typename?: 'WorkflowRun', event: string, url: string, workflow: { __typename?: 'Workflow', name: string } } | null } | null> | null } | null }
+      | { __typename: 'Tag' }
+      | { __typename: 'Tree' }
+     | null } | null };
